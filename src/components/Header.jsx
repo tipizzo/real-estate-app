7feda4ab8c26da90280962572964b8c6e105a507
KABANGA David <Davkabanga96@gmail.com>
2024-11-12 22:20:19 +0200

    Implementing the Header section and Navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Header = () => {
+  return (
+    <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' style={{backgroundImage: "url('/header_img.png')"}} id="Header">
+        <Navbar />
+        
+    </div>
+  )
+}
+
+export default Header
