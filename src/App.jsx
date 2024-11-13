d8dfdf5d49cdc0957f35df019b45483296926967
KABANGA David <Davkabanga96@gmail.com>
2024-11-13 11:59:11 +0200

    Setting up the menu for the mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header'
 
 const App = () => {
   return (
-    <div>
+    <div className='w-full overflow-hidden'>
       <Header />
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,11 +8,24 @@ const Navbar = () => {
         <img src={assets.logo} alt='' />
         <ul className='hidden md:flex gap-7 text-white'>
             <a href='#Header' className='cursor-pointer hover:text-gray-400'>Home</a>
-            <a href='#Header' className='cursor-pointer hover:text-gray-400'>About</a>
-            <a href='#Header' className='cursor-pointer hover:text-gray-400'>Projects</a>
-            <a href='#Header' className='cursor-pointer hover:text-gray-400'>Testimonials</a>
+            <a href='#About' className='cursor-pointer hover:text-gray-400'>About</a>
+            <a href='#Projects' className='cursor-pointer hover:text-gray-400'>Projects</a>
+            <a href='#Testimonials' className='cursor-pointer hover:text-gray-400'>Testimonials</a>
         </ul>
         <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign up</button>
+        <img src={assets.menu_icon} className='md:hidden w-7' alt='' />
+      </div>
+      {/* --------- mobile-menu ---------- */}
+      <div className='md:hidden fixed w-full right-0 top-0 bottom-0 overflow-hidden bg-white transition-all'>
+        <div className='flex justify-end p-6'>
+            <img src={assets.cross_icon} className='w-6' alt='' />
+        </div>
+        <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+            <a href='#Header' className='px-4 py-2 rounded-full inline-block'>Home</a>
+            <a href='#About' className='px-4 py-2 rounded-full inline-block'>About</a>
+            <a href='#Projects' className='px-4 py-2 rounded-full inline-block'>Projects</a>
+            <a href='#Testimonials' className='px-4 py-2 rounded-full inline-block'>Testimonials</a>
+        </ul>
       </div>
     </div>
   )
