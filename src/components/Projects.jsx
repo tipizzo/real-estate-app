95f936e873ee298c44761a5d324f6df9d705faed
KABANGA David <Davkabanga96@gmail.com>
2024-11-14 21:49:54 +0200

    Setting up the projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets, projectsData } from '../assets/assets'
 
 const Projects = () => {
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [cardsToShow, setCardsToShow] = useState(1);
+
+    const nextProject = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % projectsData.length)
+    }
+
+    const prevProject = () => {
+        setCurrentIndex((prevIndex) => prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1)
+    }
+
   return (
     <div className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects <span className='underline underline-offset-4 decoration-1 under font-light'>Completed</span></h1>
@@ -10,19 +22,27 @@ const Projects = () => {
       {/* slider buttons */}
 
       <div className='flex justify-end items-center mb-8'>
-        <button className='p-3 bg-gray-200 rounded mr-2' aria-label='Previous Project'>
+        <button onClick={prevProject} className='p-3 bg-gray-200 rounded mr-2' aria-label='Previous Project'>
             <img src={assets.left_arrow} alt='Previous' />
         </button>
-        <button className='p-3 bg-gray-200 rounded mr-2' aria-label='Next Project'>
+        <button onClick={nextProject} className='p-3 bg-gray-200 rounded mr-2' aria-label='Next Project'>
             <img src={assets.right_arrow} alt='Next' />
         </button>
       </div>
       {/* Project Slider Container */}
       <div className='overflow-hidden'>
-        <div className='flex gap-8 transition-transform duration-500 ease-in-out'>
+        <div className='flex gap-8 transition-transform duration-500 ease-in-out' style={{transform: `translateX(-${(currentIndex * 100) / cardsToShow}%)`}} >
             {projectsData.map((project, index) => (
                 <div key={index} className='relative flex-shrink-0 w-full sm:w-1/4'>
-                    <img src={project.image} alt={project.title} />
+                    <img src={project.image} alt={project.title} className='w-full h-auto mb-14' />
+                    <div className='absolute left-0 right-0 bottom-6 flex justify-center'>
+                        <div className='inline-block bg-white w-3/4 px-4 py-2 shadow-md'>
+                            <h2>{project.title}</h2>
+                            <p className='text-gray-500 text-sm'>{project.price} 
+                                <span> </span> {project.location}
+                            </p>
+                        </div>
+                    </div>
                 </div>
             ))}
         </div>
