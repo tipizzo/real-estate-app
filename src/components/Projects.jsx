279b785c3fc1857b4b168871003e6cc1eab68317
KABANGA David <Davkabanga96@gmail.com>
2024-11-15 17:04:56 +0200

    Implementing the framer-motion for components animation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { assets, projectsData } from '../assets/assets'
+import { motion } from 'framer-motion';
 
 const Projects = () => {
 
@@ -32,7 +33,12 @@ const Projects = () => {
     }
 
   return (
-    <div className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
+    <motion.div
+    initial={{ opacity: 0, x:-200 }}
+    transition={{ duration: 1 }}
+    whileInView={{ opacity: 1, x:0 }}
+    viewport={{ once: true }}
+    className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects <span className='underline underline-offset-4 decoration-1 under font-light'>Completed</span></h1>
       <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Crafting Spaces, Building Legacies-Explore Our Portfolio</p>
 
@@ -64,7 +70,7 @@ const Projects = () => {
             ))}
         </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
