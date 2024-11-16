fd8542264898c341571d14fc3b42a9ed49459123
KABANGA David <Davkabanga96@gmail.com>
2024-11-16 11:37:02 +0200

    Adding framer-motion to all components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { toast } from 'react-toastify';
+import { motion } from 'framer-motion';
 
 const Contact = () => {
 
@@ -32,7 +33,12 @@ const Contact = () => {
 
 
   return (
-    <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id="Contact">
+    <motion.div
+         initial={{ opacity: 0, x:-200 }}
+         transition={{ duration: 1 }}
+         whileInView={{ opacity: 1, x:0 }}
+         viewport={{ once: true }}
+         className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id="Contact">
          <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Contact <span className='underline underline-offset-4 decoration-1 under font-light'>Us</span></h1>
          <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Ready to Make a Move ? Let's Build Your Future Together</p>
 
@@ -55,7 +61,7 @@ const Contact = () => {
             <button className='bg-blue-600 text-white py-2 px-12 mb-10 rounded'>{result ? result : "Send Message"}</button>
         </form>
 
-    </div>
+    </motion.div>
   )
 }
 
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { testimonialsData } from '../assets/assets'
 import { assets } from '../assets/assets'
+import { motion } from 'framer-motion'
 
 const Testimonials = () => {
   return (
-    <div className='container mx-auto py-10 lg:px-32 w-full overflow-hidden' id='Testimonials'>
+    <motion.div
+    initial={{opacity: 0, x:100}}
+    transition={{duration: 1}}
+    whileInView={{ opacity: 1, y:0 }}
+    viewport={{once: true}}
+    className='container mx-auto py-10 lg:px-32 w-full overflow-hidden' id='Testimonials'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Customer <span className='underline underline-offset-4 decoration-1 under font-light'>Testimonials</span></h1>
       <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Real Stories from Those Who Found Home With Us</p>
 
@@ -25,7 +31,7 @@ const Testimonials = () => {
             </div>
         ))}
       </div>
-    </div>
+    </motion.div>
   )
 }
 
